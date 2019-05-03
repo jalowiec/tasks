da5eb231258bdda7493c06c4e43dc11daa1686f8
unknown <lukasz.jalowiecki@gmail.com>
2019-05-03 21:56:42 +0200

    [21.4]

--- showtasks.bat
+++ showtasks.bat
@@ -8,7 +8,7 @@ goto fail
 :runbrowser
 echo RUNCHROME
 start chrome http://localhost:8080/crud/v1/task/getTasks
-if "%ERRORLEVEL%" == "0" goto openpage
+if "%ERRORLEVEL%" == "0" goto end
 echo.
 echo RUNCHROME has errors - breaking work
 goto fail
