74858f34ae1b95aa8fca2af19c282cec0fc7b8a6
unknown <lukasz.jalowiecki@gmail.com>
2019-05-12 19:11:41 +0200

    [22.2]

---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,51 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.*;
+
+@Component
+public class TrelloClient {
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+
+    @Value("${trello.api.username}")
+    private String trelloUsername;
+
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+
+    @Value("${trello.app.token}")
+    private String trelloToken;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Optional<List<TrelloBoardDto>>  getTrelloBoards(){
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(buildUrlForBoards(), TrelloBoardDto[].class);
+        return Optional.ofNullable(Arrays.asList(boardsResponse));
+
+    }
+
+    private URI buildUrlForBoards(){
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id").build().encode().toUri();
+
+        return  url;
+    }
+
+
+
+
+}
