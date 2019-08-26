f68bb35b32232e4f72acb8a281b6d9228efcdbe9
unknown <lukasz.jalowiecki@gmail.com>
2019-08-26 18:17:23 +0200

    adding loop to the template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -22,6 +25,12 @@ public class MailCreatorService {
     private CompanyConfig companyConfig;
 
     public String buildTrelloCardEmail(String message){
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("preview_message", "Utworzenie Trello Task");
         context.setVariable("message", message);
@@ -34,6 +43,7 @@ public class MailCreatorService {
         context.setVariable("show_button", false);
         context.setVariable("is_friend", true);
         context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
